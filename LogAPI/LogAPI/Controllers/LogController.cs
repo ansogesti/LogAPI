827f909f888fe490f574928d95ff8f19a7114cb2
Алёна Пазырева <sogesti@bk.ru>
2019-10-10 22:15:38 +0300

    model

---
+++ LogAPI/LogAPI/Controllers/LogController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LogAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogController : ControllerBase
+    {
+        // PUT: api/Log/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+    }
+}
---
+++ LogAPI/LogAPI/Models/LogModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LogAPI.Models
+{
+    public class LogModel
+    {
+        public Guid ID { get; set; }
+        public DateTime EventDateTime { get; set; }
+        public string User { get; set; }
+        public string Module { get; set; }
+        public string Func { get; set; }
+        public string EventDesc { get; set; }
+
+
+    }
+}
