c518e8761e7996d7aa1a85267b9b3d76f4bb0e26
Алёна Пазырева <sogesti@bk.ru>
2019-10-10 23:00:15 +0300

    Локальная БД

---
+++ LogAPI/LogAPI/Data/Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LogAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LogAPI.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions options) : base(options)
+        {
+            //Deteled database
+            //Database.EnsureDeleted();
+
+            //Create database
+           // Database.EnsureCreated();
+        }
+
+        public DbSet<LogModel> Logs { get; set; }
+    }
+}
