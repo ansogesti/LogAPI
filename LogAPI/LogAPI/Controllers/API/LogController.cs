4a2738b4f3d5dfe35e30f99932e59d193fb2936f
Михаил <developman2013@yandex.ru>
2019-10-11 16:24:53 +0300

    hot fix

--- LogAPI/LogAPI/Controllers/API/LogController.cs
+++ LogAPI/LogAPI/Controllers/API/LogController.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using LogAPI.Data;
+﻿using LogAPI.Data;
 using LogAPI.Models;
 using LogAPI.Services;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
 namespace LogAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/Log")]
     [ApiController]
     public class LogController : ControllerBase
     {
